b8cf1c16da473d0fbefafa9a2dca6c1a622bcc19
manojpatra061 <manojpatra061@gmail.com>
2024-10-28 13:51:03 +0530

    added ES6 lib in tsconfig compilerOptions

--- app.ts
+++ app.ts
@@ -1,2 +1,22 @@
 // 1 - paste the ts code here
-// 2 - npm run start
+// 2 - npm run
+// 3 - npm run start
+
+// unknown vs any
+function fnUnknown(para: unknown): void {
+  let result = 0;
+  // result = para.length //error
+  if (typeof para === "string") {
+    result = para.length; //correct
+  }
+  console.log(result);
+}
+function fnAny(para: any): void {
+  let result = 0;
+  result = para.length; //no error
+  console.log(result);
+}
+fnUnknown("manoj"); //5
+fnUnknown([1, 2, 3]); //0
+fnAny("manoj"); //5
+fnAny([1, 2, 3]); //3
